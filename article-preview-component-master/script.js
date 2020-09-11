d41e121836c16c12d3c1f5e2c84027c36b12f1cf
nuraskiah <70048902+nuraskiah@users.noreply.github.com>
2020-09-11 11:56:15 +0800

    desktop design

---
+++ article-preview-component-master/script.js
@@ -0,0 +1,4 @@
+document.getElementById('share-btn').addEventListener('click', function () {
+  document.getElementById('buttons').classList.toggle('active');
+  document.getElementById('share-btn').classList.toggle('active');
+});
